e15e6d7286ceaa2b3d084f72e182ffac744bc7a2
Steven Crouse <sdcrouse91@gmail.com>
2018-08-04 02:01:17 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,6 @@
+require 'pry'
 def unsafe?(speed)
+  binding.pry
   if speed < 40 || speed > 60
     true 
   else
@@ -12,6 +14,7 @@ end
 
 
 def not_safe?(speed)
+  binding.pry
 	speed < 40 || speed > 60 ? true : false
 	
 	#Another variation (equivalent to if -> elsif -> else):
